95e952a2699b4c3e226b7cd6e3d9403f97d815d4
Murilo <murilodesousag@gmail.com>
2023-01-06 09:55:54 -0300

    estilização feitas durante feedback

--- pokedex/src/Components/PokeCard/PokeCard.js
+++ pokedex/src/Components/PokeCard/PokeCard.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { goToPokemonDatails } from '../../Router/coodinator';
-import { PokeCardContainer } from './PokecardStyle';
+import { ImagePokemon, PId, PokeButtonContainer, PokeCardContainer, PokeInfoContainer, PokeTypesContainer } from './PokecardStyle';
 import {PokemonsContext} from '../../Global/GlobalContext'
 import axios from 'axios';
 import { URL_BASE } from '../../constants/contanst';
@@ -26,22 +26,25 @@ export default function PokeCard(props) {
         console.log(err)
       })
   },[])
+  console.log(pokemon.spri)
 
  return (
    <PokeCardContainer>
-     <div>
-       <p>#{pokemon && pokemon.id}</p>
-       <p>{pokemon && pokemon.name}</p>
-
-       {pokemon.types && pokemon.types.map((type)=>{
-         return <span>{type.type.name} - </span>
-       })}
-       <img src={pokemon.sprites.other[`official-artwork`].front_default}/>
-     </div>
-     <div>
-      <button>Capturar</button>
+     <PokeInfoContainer>
+       <PId>#{pokemon && pokemon.id}</PId>
+       <h1>{pokemon && pokemon.name}</h1>
+       <PokeTypesContainer>
+          {pokemon.types && pokemon.types.map((type)=>{
+            return <span>{type.type.name} - </span>
+          })}
+        </PokeTypesContainer>
+       </PokeInfoContainer>
+          <ImagePokemon src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/251.png"/>
+          {/* <img src={pokemon.sprites.other[`official-artwork`].front_default}/> */}
+     <PokeButtonContainer>
+      <a href=""><strong>Capturar</strong></a>
       <button onClick={()=>{goToPokemonDatails(navigate, id)}}>Detalhes</button>
-     </div>
+     </PokeButtonContainer>
 
    </PokeCardContainer>
   );
--- pokedex/src/Components/PokeCard/PokecardStyle.js
+++ pokedex/src/Components/PokeCard/PokecardStyle.js
@@ -7,4 +7,40 @@ export const PokeCardContainer = styled.section`
     align-items: center;
     padding: 10px;
     margin: 10px 0;
+    background: green;
+    color: #FFFFFF;
 `;
+
+export const PokeInfoContainer = styled.section`
+    border: 1px solid black;
+    display: flex;
+    /* flex-direction: column; */
+    align-items: center;
+    background: blue;
+
+`
+
+export const PokeButtonContainer = styled.section`
+    border: 1px solid black;
+    padding: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 80%;
+    align-items: center;
+    color: black;
+`
+
+
+export const PokeTypesContainer = styled.section`
+
+`
+
+export const PId = styled.p`
+    
+`
+
+export const ImagePokemon = styled.img`
+    width: 200px;
+    height: 200px;
+`
